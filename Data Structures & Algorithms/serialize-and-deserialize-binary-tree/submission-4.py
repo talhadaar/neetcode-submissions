98acae20090a4563381f0d44c05676e5eeb29dbd
talha <muhammadtalhadar@gmail.com>
2026-05-20 16:47:25 +0500

    Add: serialize-and-deserialize-binary-tree - submission-4

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-4.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        # Level order traversal
+        if not root:
+            return "N"
+
+        queue = deque([root])
+        res = []
+
+        while queue:
+            top = queue.popleft()
+            if not top:
+                res.append("N")
+            else:
+                res.append(str(top.val))
+                queue.append(top.left)
+                queue.append(top.right)
+        return ",".join(res)
+
+
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        if vals[0] == "N":
+            return None
+
+        root = TreeNode(int(vals[0]))
+        self.i = 1
+        queue = deque([root])
+
+        while queue:
+            top = queue.popleft()
+            if vals[self.i] != "N":
+                top.left = TreeNode(int(vals[self.i]))
+                queue.append(top.left)
+            self.i +=1
+            if vals[self.i] != "N":
+                top.right = TreeNode(int(vals[self.i]))
+                queue.append(top.right)
+            self.i += 1
+        
+        return root
