9d8a80c6750bc154063901daf7af134b404c83c5
talha <muhammadtalhadar@gmail.com>
2026-08-18 13:02:01 +0500

    Add: search-2d-matrix - submission-7

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-7.py
@@ -0,0 +1,39 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+
+        # staircase search: O(M+N) at worst, could give all vertically and horizontally for target
+
+        # Binary Search
+        # 2 pass: Narrow and binary search: Find a row that is a suitable rage with BS, search row with BS
+        # 1 pass: Linearize coords and do a binary search: Whole thing O(log(n))
+
+
+        # find a row
+        top,bottom = 0, ROWS-1
+        while top<=bottom:
+            mid = (top + bottom) // 2
+            if target > matrix[mid][-1]:
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bottom = mid - 1
+            else:
+                break
+
+        # if we didnt converge or a row, a valid one doesn't exist
+        if not (top<=bottom):
+            return False
+
+        # now we BS on the row
+        l,r = 0, COLS - 1
+        row = (top+bottom)//2
+        while l<=r:
+            mid =(l+r) // 2
+            if target < matrix[row][mid]:
+                r = mid - 1
+            elif target > matrix[row][mid]:
+                l = mid + 1
+            else:
+                return True
+        return False
