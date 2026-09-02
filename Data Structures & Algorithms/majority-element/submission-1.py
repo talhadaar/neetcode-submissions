8a41f8f87ac1511aed1d1baa7985463ba6762e5e
talha <muhammadtalhadar@gmail.com>
2026-09-02 18:32:49 +0500

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Sort and return mid+1'th element O(nlog(n)) and no space
+        # Hashmap: if element appears n/2 + 1 times, we found it O(N),O(N)
+
+        # Attempt to pick a candidate from a pool of votes.
+        # Candidate with most votes wins.
+        # if a candidate gets out voted, pick the next one and repeat.
+        # Candidate with most votes will survive.
+        # This way we keep a running score of candidates
+        # where scores cancel each other out, until candidate with maximum score is left
+        # Candidate with more picks survives each time
+
+        # Alternatively: Probability of picking majority candidate is >50%
+        # Randomly pick candidates, if one is picked 6 out of 10 tries, that is the majority one
+        votes,candidate = 0,nums[0]
+        for num in nums:
+            if num==candidate:
+                votes+=1
+            else:
+                votes-=1
+
+            if votes == 0:
+                candidate = num
+                votes = 1
+        return candidate
