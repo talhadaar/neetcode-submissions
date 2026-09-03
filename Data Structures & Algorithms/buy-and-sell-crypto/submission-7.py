c3b3eca0b2c4f4f712ab307c277befb57d99525c
talha <muhammadtalhadar@gmail.com>
2026-09-03 16:13:24 +0500

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Brute Force: Extend a pointer from eacn prices[i]
+        # O(N*N)
+
+        res, low = 0, float('inf')
+        for p in prices:
+            low = min(low, p)
+            res = max(res, p - low)
+        return res
