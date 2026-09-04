b97f31880dac12ea1bcc3569b96cb969983d0816
talha <muhammadtalhadar@gmail.com>
2026-09-04 15:31:34 +0500

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # is intervals sorted? If yes, just merge, if not, sort and merge
+        # After sorting, if sj<=ei: then merge. merged[si, max(ei,ej)]
+
+        if not intervals: return []
+
+        intervals.sort(key=lambda pair: pair[0])
+        res = [intervals[0]]
+        
+        for sj,ej in intervals:
+            ei = res[-1][1]
+
+            if sj<=ei:
+                res[-1][1] = max(ei, ej)
+            else:
+                res.append([sj,ej])
+        return res
