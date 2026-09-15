9c9dc280f142fe2ff5d8fca8e6b39f3b02df82ea
talha <muhammadtalhadar@gmail.com>
2026-09-15 16:58:17 +0500

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # Iterative DFS
+        # PostOrder Traversal: LC,RC,P
+
+        stack = [root] if root else []
+        # key: node, valid: (height, bestDiameter)
+        cache = {None: (0,0)}
+
+        while stack:
+            # Do no pop since we need the height
+            curr = stack[-1]
+
+            if curr.left and curr.left not in cache:
+                stack.append(curr.left)
+            elif curr.right and curr.right not in cache:
+                stack.append(curr.right)
+            else:
+                curr = stack.pop()
+
+                lh,ld = cache[curr.left]
+                rh,rd = cache[curr.right]
+
+                currDiameter = lh+rh
+                cache[curr] = (1+max(lh,rh), max(currDiameter, rd,ld))
+
+        return cache[root][1]
